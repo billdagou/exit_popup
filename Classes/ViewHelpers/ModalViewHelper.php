e213ac386973bc7f8854491f288f48597277c65c
Bill.Dagou <billdagou@gmail.com>
2022-06-05 14:50:05 +0800

    TYPO3 v10.5

--- Classes/ViewHelpers/ModalViewHelper.php
+++ Classes/ViewHelpers/ModalViewHelper.php
@@ -1,39 +1,48 @@
 <?php
 namespace Dagou\ExitPopup\ViewHelpers;
 
+use TYPO3\CMS\Core\Page\AssetCollector;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
-class ModalViewHelper extends AbstractTagBasedViewHelper {
+class ModalViewHelper extends AbstractViewHelper {
     /**
      * @var bool
      */
     protected $escapeChildren = FALSE;
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = FALSE;
+    /**
+     * @var \TYPO3\CMS\Core\Page\AssetCollector
+     */
+    protected $assetCollector;
+
+    /**
+     * @param \TYPO3\CMS\Core\Page\AssetCollector $assetCollector
+     */
+    public function injectAssetCollector(AssetCollector $assetCollector) {
+        $this->assetCollector = $assetCollector;
+    }
 
     public function initializeArguments() {
         $this->registerArgument('target', 'string', 'Modal identifier', TRUE);
         $this->registerArgument('name', 'string', 'Cookie name', FALSE, 'expop');
-        $this->registerArgument('expires', 'int', 'Expiring days', FALSE, 3);
-        $this->registerArgument('footer', 'boolean', 'Add to footer or not.', FALSE, TRUE);
+        $this->registerArgument('expires', 'int', 'Expiring days', FALSE, 7);
     }
 
-    public function render() {
+    /**
+     * @return string
+     */
+    public function render(): string {
         if ($_COOKIE[$this->arguments['name']]) {
             return '';
         }
 
-        $footer = $this->arguments['footer'];
-        unset($this->arguments['footer']);
-
-        $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-        if ($footer) {
-            $pageRenderer->addJsFooterLibrary('exit_popup', 'EXT:exit_popup/Resources/Public/Javascript/main.js');
-            $pageRenderer->addJsFooterInlineCode('exit_popup-config', 'var expop = '.json_encode($this->arguments).';');
-        } else {
-            $pageRenderer->addJsLibrary('exit_popup', 'EXT:exit_popup/Resources/Public/Javascript/main.js');
-            $pageRenderer->addJsFooterInlineCode('exit_popup-config', 'var expop = '.json_encode($this->arguments).';');
-        }
+        $this->assetCollector->addJavaScript('exit_popup', 'EXT:exit_popup/Resources/Public/Javascript/main.js');
+        $this->assetCollector->addInlineJavaScript('exit_popup-config', 'var expop = '.json_encode($this->arguments).';');
 
         return $this->renderChildren();
     }
